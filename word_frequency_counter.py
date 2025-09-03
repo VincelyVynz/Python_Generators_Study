c1fa64edb1c23cea510592548760d21cf6150970
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-03 16:18:33 +0530

    Try to convert word frequency counter function to a class

--- word_frequency_counter.py
+++ word_frequency_counter.py
@@ -31,3 +31,43 @@ def get_word_frequency(filename):
 
     return frequency_dict
 
+# ---------------- Change to Class ---------------- #
+
+class WordFrequency:
+    def __init__(self, filename):
+        self.filename = filename
+        self.freq_dict = {}
+        self.line_reader()
+        self.word_splitter()
+
+    def line_reader(self):
+        try:
+            with open(self.filename, 'r') as file:
+                self.line = file.readline()
+                while self.line != "":
+                    yield self.line
+                    self.line = file.readline()
+        except FileNotFoundError:
+            print(f"Could not fine {self.filename}")
+        except Exception as e:
+            print(e)
+
+    def word_splitter(self):
+        punctuations = r".,!?;:\"'()[]{}"
+        while self.line != "":
+            for word in self.line:
+                self.word = word.split().strip(punctuations)
+                yield self.word
+                self.freq_counter()
+
+    def freq_counter(self.word):
+        if self.word not in self.freq_dict:
+            self.freq_dict[self.word] = 1
+        else:
+            self.freq_dict[self.word] += 1
+
+    def print_freq_dict(self):
+        if self.word not in self.freq_list:
+            self.freq_counter()
+        print(self.freq_dict)
+
